fddf6e524d1275f682c3c1b9a02f4c8d9dfb1139
rpz <rippa80@gmail.com>
2015-03-31 00:33:10 +0300

    done some...

--- bus_network/bn.py
+++ bus_network/bn.py
@@ -10,25 +10,40 @@ stops_re = re.compile(r'R\d+=\[(.+)\]')
 def add_to_graph(g, stops, ind):
     if len(g) != 0:
         for s in stops:
-            g[0].append([False, ind, s])
-            g.append([[False, ind, s]])
+            g[0].append([100000, ind, s])
+            g.append([[100000, ind, s, False]])
     else:
         g.append([[False, ind, s] for s in stops])
         for s in stops:
             g.append([[False, ind, s]])
 
-    j = 1
-    for row in g[1:]:
-        for i in range(1, len(g[0])):
-            if i == j:
-                row.append(-1)
-            else:
-
-
 
 def find_shortest_path(g, start, stop):
-    for row in g:
-        print(row)
+    results = []
+    relax_start_node(start)
+    while 1:
+        n = find_min_node()
+        siblings = find_siblings(n)
+        if len(siblings) == 0:
+            return results
+        relax(siblings)
+        stop_node = find_stop(stop)
+        if stop_node is not None:
+            results.append(distance(stop_node))
+
+
+def fill_graph(g):
+    for i in range(1, len(g)):
+        for j in range(1, len(g[0])):
+            if i == j:
+                g[i].append(1)
+            else:
+                if g[i][0][1] == g[0][j][1] and abs(g[i][0][2] - g[0][j][2]) == 1:
+                    g[i].append(7)
+                elif g[i][0][2] == g[0][j][2]:
+                        g[i].append(12)
+                else:
+                    g[i].append(-1)
 
 
 def main():
@@ -52,6 +67,7 @@ def main():
                 add_to_graph(graph, stops, index)
                 index += 1
 
+            fill_graph(graph)
             find_shortest_path(graph, start, stop)
 
 
