cd753c6031d300321a4c5f3e2bf685f69ebca913
rpz <rippa80@gmail.com>
2015-04-09 23:25:03 +0300

    started cpp version

--- bus_network/cpp/bn.cpp
+++ bus_network/cpp/bn.cpp
@@ -1,23 +1,64 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
+#include <fstream>
+#include <regex>
+#include <cstdio>
+#include <string>
+#include <cstdint>
+#include <vector>
+
+namespace bn {
+
+  const size_t max_size = 10000;
+
+  class graph {
+  private:
+    struct node {
+      uint16_t label;
+      uint16_t dist;
+      uint16_t index;
+      uint8_t route;
+      bool visited;
+    };
+  public:
+    void print() {
+	    for (size_t i = 0; i < nodes_.size(); ++i) {
+        const node& n = nodes_[i];
+        std::printf("n{%-2u, %-2u, %-2u, %-2u, %-2u}", 
+                    n.route, n.label, n.dist, 
+                    n.index, n.visited);
+
+		    for (size_t j = matrix_size_ * i; j < matrix_size_ * (i+1); ++j) {
+			    std::printf("%3d", matrix_[j]);
+		    }
+		    std::printf("\n");
+	    }
+    }
+
+    void connect_nodes() {
+    }
+  private:
+    std::vector<node> nodes_;
+    std::vector<int8_t> matrix_;
+    uint16_t matrix_size_;
+  };
+}
 
 int main(int argc, const char* argv[]) {
   if (argc < 2) {
-    printf("Usage: bn <file>\n");
+    std::printf("Usage: bn <file>\n");
     return 1;
   }
-  
-  FILE* f = fopen(argv[1], "rb");
-  if (f == NULL) {
-    printf("File open error\n");
-    return 1;
-  }
-  fseek(f, 0, SEEK_END);
-  uint32_t fsize = ftell(f);
-  fseek(f, 0 , SEEK_SET);
 
-  char* buf = (char*)malloc(fsize + 1);
-  free(buf);
-  return 0;
+  std::ifstream ifs(argv[1]);
+  if (!ifs) {
+    std::printf("File open error\n");
+    return 1;
+  }
+
+  std::string line;
+  line.reserve(1024);
+
+  while (!ifs.eof()) {
+    std::getline(ifs, line);
+    bn::process(line);
+  }
 }
