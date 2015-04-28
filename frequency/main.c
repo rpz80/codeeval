b4746eb06357829619b75f1c82395bb70c6f0e63
rpz80 <rippa80@gmail.com>
2015-04-28 19:48:59 +0300

    another try

--- frequency/main.c
+++ frequency/main.c
@@ -12,17 +12,61 @@
 #include <sys/io.h>
 #include <sys/mman.h>
 
+#define CHANGE_LIMIT 10
+
+enum direction_type {
+  ascending,
+  descending,
+  no
+};
+
 static
 void process(char* data, const uint32_t size) {
   uint32_t count = 0;
   int parsing = 1;
+  int first_hit = 1;
+  int tmp, local_peak=0, change_count=0;
+  enum direction_type dir = no;
 
   for (uint32_t i = 0; i < size; ++i) {
     if (isdigit(*data) || *data == '-') {
       if (parsing) {
         parsing = 0;
-        strtoul(data, NULL, 10);
+        tmp = strtoul(data, NULL, 10);
+        if (first_hit) {
+          first_hit = 0;
+          local_peak = tmp;
+        }
+        else {
+          if (tmp - local_peak > 10) {
+            //printf("ascening\n");
+            if (dir == no) {
+              dir = ascending;
+            }
+            else if (dir == descending) {
+              dir = ascending;
+              ++change_count;
+            }
+            local_peak = tmp;
+          }
+          else if (local_peak - tmp > 10) {
+            //printf("descending\n");
+            if (dir == no) {
+              dir = descending;
+            }
+            else if (dir == ascending) {
+              dir = descending;
+              ++change_count;
+            }
+            local_peak = tmp;
+          }
+        }
         ++count;
+        if (count % 2000 == 0) {
+          printf("change count: %u\n", 10*change_count/2);
+          first_hit = 1;
+          change_count = 0;
+        }
       }
     }
     else {
